69e92887f20c9efcada1b10996fc5388e7854a20
Blaser993 <corradodepinto.design@gmail.com>
2023-09-06 15:28:03 +0200

    snack5 completato

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -2,13 +2,13 @@
 Console.WriteLine("Hello, World!");
 
 
-//Snack 4: Calcola la somma e la media dei numeri da 2 a 10.
-//Snack 5: Il software chiede all’utente di inserire un numero. Se il numero inserito è pari, stampa il numero, se è dispari, stampa il numero successivo.
 //Snack 6: In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby. Chiedi all’utente il suo nome e comunicagli se può partecipare o meno alla festa.
 //Snack 7: Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
 //Snack 8: Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.
 //Snack 9: Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array, fino a quando la somma degli elementi è minore di 50.
 //Snack 10: Fai inserire un numero, che chiameremo N, all’utente. Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100. Ogni volta che ne crei uno, stampalo a schermo.
+
+
 
 
 //Snack 1: L’utente inserisce due numeri in successione. Il software stampa il maggiore.
@@ -81,5 +81,23 @@ for (int i = 0; i < numeriDa2a10.Length; i++)
 mediaNumeriDa2a10 = sommaNumeriDa2a10 / numeriDa2a10.Length;
 
 Console.WriteLine($"La somma dei numeri da 2 a 10 è {sommaNumeriDa2a10}, la loro media è {mediaNumeriDa2a10}");
+
+Console.WriteLine();
+
+
+//Snack 5: Il software chiede all’utente di inserire un numero. Se il numero inserito è pari, stampa il numero, se è dispari, stampa il numero successivo.
+
+Console.Write("Inserisci un numero: ");
+int numeroPari = Convert.ToInt32(Console.ReadLine());
+
+if (numeroPari % 2 == 0)
+{
+    Console.WriteLine($"Il numero {numeroPari} che hai scritto è pari");
+}
+else
+{
+    int numeroDispari = numeroPari + 1;
+    Console.WriteLine($"il numero che hai scritto è dispari, ecco il suo successivo: {numeroDispari}");
+}
 
 Console.WriteLine();
